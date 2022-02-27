5515a5868f6425a32118b9bd0f01d1f41fb0b076
Yusufgokx <yusuf.fb.25@gmail.com>
2022-02-28 00:37:38 +0300

    gg
    gg

---
+++ Assets/Karakter/Kodlar/KarakterKontroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarakterKontroller : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //sdgsd
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
