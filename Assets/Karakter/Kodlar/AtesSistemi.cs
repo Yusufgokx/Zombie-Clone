b8dc3c43c1ca062c551c46541ad08e5cfb734741
Yusufgokx <yusuf.fb.25@gmail.com>
2022-03-05 20:08:06 +0300

    Animation,Animator,Damage Sistems
    animasyonların düzenlenmesi zombi navigasyon işlemleri karakter takip

---
+++ Assets/Karakter/Kodlar/AtesSistemi.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtesSistemi : MonoBehaviour
+{
+    Camera kamera;
+    public LayerMask zombikatman;
+    void Start()
+    {
+        kamera = Camera.main;
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+
+            AtesEtme();
+
+
+        } 
+                
+    }
+
+    void AtesEtme()
+    {
+        Ray ray = kamera.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit, Mathf.Infinity, zombikatman))
+        {
+            hit.collider.gameObject.GetComponent<Zombie>().HasarAL();
+        }
+
+    }
+}
--- Assets/Zombie/KodlarZombie/Zombie.cs
+++ Assets/Zombie/KodlarZombie/Zombie.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
     public float zombiHP=100;
     Animator ZombiAnim;
     bool zombiOLU;
-     
 
+    private float mesafe;
+    public float kovalamaMesafe;
+    public float SaldiriMesafe;
+    NavMeshAgent zombiNavMesh;
+
+    GameObject hedefOyuncu;
     void Start()
     {
         ZombiAnim = this.GetComponent<Animator>();
+        hedefOyuncu = GameObject.Find("SWAT");
+        zombiNavMesh = this.GetComponent<NavMeshAgent>();
     }
 
    
     void Update()
     {
+        this.transform.LookAt(hedefOyuncu.transform.position);
         if (zombiHP <= 0)
         {
             zombiOLU = true;
@@ -28,10 +37,42 @@ public class Zombie : MonoBehaviour
         }
         else
         {
-            //İLERİDE HAREKET KODU BURAYA 
+            mesafe = Vector3.Distance(this.transform.position, hedefOyuncu.transform.position);
+            if (mesafe < kovalamaMesafe) 
+            {
+                zombiNavMesh.isStopped = false;
+                zombiNavMesh.SetDestination(hedefOyuncu.transform.position);
+                ZombiAnim.SetBool("yuruyor",true);
+                ZombiAnim.SetBool("saldiriyor", false);
+                this.transform.LookAt(hedefOyuncu.transform.position);
+                
+
+
+            }
+            else
+            {
+                zombiNavMesh.isStopped = true ;
+                ZombiAnim.SetBool("yuruyor", false);
+                ZombiAnim.SetBool("saldiriyor", false);
+                
+            }
+            if (mesafe < SaldiriMesafe) 
+            {
+                this.transform.LookAt(hedefOyuncu.transform.position);
+                zombiNavMesh.isStopped = true;
+                ZombiAnim.SetBool("yuruyor", false);
+                ZombiAnim.SetBool("saldiriyor", true);
+                
+                
+            }
+
+
         }
     }
-
+    public void HasarVer()
+    {
+        //hedefOyuncu.GetComponent<KarakterKontroller>().HasarAL();
+    }
 
     IEnumerator YokOL()
     {
