c9b5af96458ad129501e82baa8318bb386a288a9
Yusufgokx <yusuf.fb.25@gmail.com>
2022-03-09 00:16:42 +0300

    animatör,animation islemleri
    sarjor mermi degisme islemleri animasyonlarla baglama.

--- Assets/Karakter/Kodlar/AtesSistemi.cs
+++ Assets/Karakter/Kodlar/AtesSistemi.cs
@@ -9,36 +9,66 @@ public class AtesSistemi : MonoBehaviour
     public KarakterKontroller hpKontrol;
     Animator anim;
 
+    public ParticleSystem muzzleFlash;
+
+    private float sarjor = 5;
+    private float cephane = 10;
+    private float sarjorKapasitesi = 5;
+
     void Start()
     {
         kamera = Camera.main;
         anim =this.gameObject.GetComponent<Animator>();
-       
+        muzzleFlash.Stop();
+
     }
 
     
     void Update()
     {
 
-        if (hpKontrol.YasiyorMu()==true)
+        if (hpKontrol.YasiyorMu() == true)
         {
             if (Input.GetMouseButton(0))
             {
+                if (sarjor > 0)
+                {
+                    anim.SetBool("atesEt", true);
+                }
+                if (sarjor <= 0)
+                {
+                    anim.SetBool("atesEt", false);
+                }
+                if (sarjor <= 0 && cephane > 0)
+                {
+                    anim.SetBool("sarjorDegistirme",true);
+               
+                }
 
-                anim.SetBool("atesEt",true);
             }
-            if (Input.GetMouseButtonUp(0))
+            else
             {
                 anim.SetBool("atesEt", false);
             }
-        }   
-        
+
+            /*if (Input.GetMouseButtonUp(0))
+            {
+                anim.SetBool("atesEt", false);
+            }
+           */
+        }
 
     }
-
+    public void SarjorDegistirme()
+    {
+        cephane -= sarjorKapasitesi - sarjor;
+        sarjor = sarjorKapasitesi;
+        anim.SetBool("sarjorDegistirme", false);
+    }
     public void AtesEtme()
     {
-        
+        sarjor--;
+        muzzleFlash.Play();
         Ray ray = kamera.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
         RaycastHit hit;
         if(Physics.Raycast(ray,out hit, Mathf.Infinity, zombikatman))
